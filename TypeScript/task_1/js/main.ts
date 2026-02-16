f6bface98632bf8ac406e278eeaa88952c93018f
Yannick <yannicksookree@gmail.com>
2026-02-16 12:55:01 +0400

    upd

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -52,3 +52,38 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+// Define an interface for the constructor
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Define an interface for the StudentClass
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student: StudentClassInterface = new StudentClass("John", "Doe");
+console.log(student.displayName());
+console.log(student.workOnHomework());
